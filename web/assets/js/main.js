2b69a6b77f1b2913365f16b140ce9ee3f3e558b0
Thalisson Felipe <thalissonlipe@gmail.com>
2020-08-09 13:56:38 -0300

    adding switch theme js function

---
+++ web/assets/js/main.js
@@ -0,0 +1,20 @@
+function setTheme(theme) {
+    localStorage.setItem('theme', theme);
+    document.documentElement.className = theme;
+}
+
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}
+
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
