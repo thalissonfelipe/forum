b0f01761fec02b053c4a9af4340b357606a31410
danielfilhocoelho <danielfilhocoelho@users.noreply.github.com>
2020-08-18 10:23:23 -0300

    Adicionando Comment Controller

---
+++ server/src/controllers/CommentsController.js
@@ -0,0 +1,93 @@
+const Comment = require('../models/Comment');
+const APIFeatures = require('../utils/apiFeatures');
+
+class CommentsController {
+
+    async createComment (request, response) {
+        try{
+            const comment = await Comment.findByIdAndUpdate(request.params.id,request.body,{
+                new: true,
+                runValidators: true
+            });
+            response.status(200).json({
+                status : "Success",
+                data: {
+                    comment : comment
+                }
+            });
+        }
+        catch (err){
+            response.status(404).json({
+                status: "Fail",
+                message: err
+            });
+        }
+    }
+    async updateComment (request, response) {
+        try{
+            const comment = await Comment.findByIdAndUpdate(request.params.id,request.body,{
+                new: true,
+                runValidators: true
+            });
+            response.status(200).json({
+                status : "Success",
+                data: {
+                    comment : comment
+                }
+            });
+        }
+        catch (err){
+            response.status(404).json({
+                status: "Fail",
+                message: err
+            });
+        }
+        
+    }
+    async showAllComments(request, response){
+        try{
+            // EXECUTE QUERY
+            const features = new APIFeatures(Comment.find(),request.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+            const comments = await features.query;
+            // SEND RESPONSE
+            response.status(200).json({
+                status: "Success",
+                requestedAt : request.requestTime,
+                results: comments.length,
+                data: {
+                    tours : comments
+                }
+            });
+        }
+        catch (err){
+            response.status(404).json({
+                status: "Fail",
+                message: err
+            });
+        }
+
+    }
+
+    async deleteComment (request, response) {
+
+        try{
+            await Comment.findByIdAndDelete(request.params.id);
+            response.status(200).json({
+                status : "Success",
+                data: null
+            });
+        }
+        catch (err){
+            response.status(404).json({
+                status: "Fail",
+                message: err
+            });
+        }
+    }
+}
+
+module.exports = CommentsController;
