d085b7e928aee5db96f1af6cda98485e41e7af66
danielfilhocoelho <danielfilhocoelho@users.noreply.github.com>
2020-08-19 10:23:10 -0300

    Rotas dos posts

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const routes = express.Router();
 const UsersController = require('./controllers/UsersController');
 const AuthController = require('./controllers/AuthController');
+const PostsController = require('./controllers/PostsController');
 const middlewares = require('./controllers/middlewares');
 
 const usersController = new UsersController();
 const authController = new AuthController();
+const postsController = new PostsController();
 
 // User routes
 routes.get('/users', middlewares, usersController.index);
@@ -16,4 +18,14 @@ routes.post('/users/logout', authController.logout);
 routes.put('/users/:registry', usersController.update);
 routes.delete('/users/:registry', middlewares, usersController.destroy);
 
+//Post routes
+
+routes.route('/posts')
+    .get(postsController.showAllPosts)
+    .post(postsController.createPost);
+routes.route('/posts/:id')
+    .patch(postsController.updatePost)
+    .delete(postsController.deletePost);
+
 module.exports = routes;
+
